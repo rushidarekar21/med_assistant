1618ab65ecbc74a0eeac0384b7370915034cdd0f
RushiDarekar <150501236+rushidarekar21@users.noreply.github.com>
2024-08-31 15:32:35 +0530

    Update healthcare_asistant.py

--- healthcare_asistant.py
+++ healthcare_asistant.py
@@ -1,48 +1,7 @@
 import streamlit as st
 import requests
-import threading
-from fastapi import FastAPI
-import uvicorn
-from langchain_core.prompts import ChatPromptTemplate
-from langserve import add_routes
-from langchain_groq import ChatGroq
 
-# API key and LLM setup
-api_key = "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"
-
-llm = ChatGroq(
-    model="llama-3.1-70b-versatile",
-    temperature=1,
-    groq_api_key=api_key
-)
-
-app = FastAPI(
-    title="Simple Server",
-    version='1.0',
-    description="My first API server"
-)
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful assistant. Please respond to the user queries."),
-        ("user", "Question:{question}")
-    ]
-)
-
-add_routes(
-    app,
-    prompt | llm,
-    path="/prompt"
-)
-
-# Function to run the FastAPI server in a thread
-def run_fastapi():
-    uvicorn.run(app, host='0.0.0.0', port=8000)
-
-# Start the FastAPI server in a separate thread
-threading.Thread(target=run_fastapi, daemon=True).start()
-
-# Streamlit app
+# Set the title of the Streamlit app
 st.title('Speech Therapy Assistant')
 
 # Create a text input widget for the user to enter a prompt
